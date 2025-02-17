85704a9022f37e469c34dfb56df8244f457cb78c
ssweilee <119922122+ssweilee@users.noreply.github.com>
2025-02-17 03:19:26 +0000

    edit font and arrow

--- lib/funfact_page.dart
+++ lib/funfact_page.dart
@@ -17,12 +17,26 @@ class Funfact extends StatelessWidget {
   }
 }
 
-class RecyclingPage extends StatelessWidget {
+class RecyclingPage extends StatefulWidget {
+  @override
+  _RecyclingPageState createState() => _RecyclingPageState();
+}
+
+class _RecyclingPageState extends State<RecyclingPage> {
+  final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('How to Sort Your Recyclables'),
+        title: Text(
+          'How to Sort Your Recyclables',
+          style: TextStyle(
+            fontSize: 28,
+            fontFamily: 'RinsHandwriting',
+            fontWeight: FontWeight.bold, 
+            ),
+        ),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
@@ -32,31 +46,99 @@ class RecyclingPage extends StatelessWidget {
             SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                spacing: 20, 
-                runSpacing: 10, 
-                children: [
-                  RecyclingItem(imageUrl: 'assets/bins/green_wheelie_bin.png', title: 'Green wheelie bin', description: 'Garden waste'),
-                  RecyclingItem(imageUrl: 'assets/bins/black_wheelie_bin.png', title: 'Black wheelie bin', description: 'Non-recyclable waste'),
-                  RecyclingItem(imageUrl: 'assets/bins/food_waste.png', title: 'Brown bin', description: 'Food waste'),
-                  RecyclingItem(imageUrl: 'assets/bins/green_box.png', title: 'Green box', description: 'Plastic and metal'),
-                  RecyclingItem(imageUrl: 'assets/bins/blue_bag.png', title: 'Blue bag', description: 'Cardboard and brown paper'),
-                  RecyclingItem(imageUrl: 'assets/bins/black_box.png', title: 'Black box', description: 'Glass, paper, and others'),
-                ],
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, 
+                  mainAxisSpacing: 10, 
+                  crossAxisSpacing: 20, 
+                  childAspectRatio: 1, 
+                ),
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  final items = [
+                    RecyclingItem(imageUrl: 'assets/bins/green_wheelie_bin.png', title: 'Green wheelie bin:', description: 'Garden waste'),
+                    RecyclingItem(imageUrl: 'assets/bins/black_wheelie_bin.png', title: 'Black wheelie bin:', description: 'Non-recyclable waste'),
+                    RecyclingItem(imageUrl: 'assets/bins/food_waste.png', title: 'Brown bin:', description: 'Food waste'),
+                    RecyclingItem(imageUrl: 'assets/bins/green_box.png', title: 'Green box:', description: 'Plastic and metal'),
+                    RecyclingItem(imageUrl: 'assets/bins/blue_bag.png', title: 'Blue bag:', description: 'Cardboard and brown paper'),
+                    RecyclingItem(imageUrl: 'assets/bins/black_box.png', title: 'Black box:', description: 'Glass, paper, and others'),
+                  ];
+                  return items[index];
+                },
               ),
             ),
             SizedBox(height: 20),
             Text(
               '15 RECYCLING FUN FACTS!',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold, 
+                fontFamily: 'RinsHandwriting',
+              ),
             ),
             SizedBox(height: 20),
-            Center(
-              child: Image.asset('assets/arrow/arrow.png', width: 158, height: 79), 
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset('assets/arrow/arrow.png', width: 683, height: 79),
+                Positioned(
+                  left: 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      if (_pageController.page! > 0) {
+                        _pageController.previousPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                        );
+                      }
+                    },
+                    child: Container(
+                      width: 79, 
+                      height: 79,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      if (_pageController.page! < 14) {
+                        _pageController.nextPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                        );
+                      }
+                    },
+                    child: Container(
+                      width: 79, 
+                      height: 79,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  height: 300, 
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: 15,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/funfacts/funfact_${index + 1}.png',
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 10),
-            RecyclingFactsCarousel(),
           ],
         ),
       ),
@@ -78,39 +160,21 @@ class RecyclingItem extends StatelessWidget {
         Image.asset(imageUrl, width: 100, height: 100),
         Text(
           title,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+          style: TextStyle(
+            fontWeight: FontWeight.normal, 
+            fontSize: 14,
+            fontFamily: 'RinsHandwriting',
+          ),
         ),
-        Text(description, textAlign: TextAlign.center),
+        Text(
+          description,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.normal, 
+            fontFamily: 'RinsHandwriting',
+          ),
+        ),
       ],
-    );
-  }
-}
-
-
-class RecyclingFactsCarousel extends StatelessWidget {
-  final List<String> imagePaths = List.generate(
-    15,
-    (index) => 'assets/funfacts/funfact_${index + 1}.png',
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 568, 
-      child: PageView.builder(
-        itemCount: imagePaths.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset(
-              imagePaths[index],
-              width: 568,
-              height: 568,
-              fit: BoxFit.cover, 
-            ),
-          );
-        },
-      ),
     );
   }
 }
